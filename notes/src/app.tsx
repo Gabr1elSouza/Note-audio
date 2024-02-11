e470c875ea0f0bf3a4f6ec1b4e75dba24700ad2c
Gabriel Souza <126912751+Gabr1elSouza@users.noreply.github.com>
2024-02-10 23:43:10 -0300

    States and Components

--- notes/src/app.tsx
+++ notes/src/app.tsx
@@ -1,9 +1,14 @@
 import { NewNoteCard } from "./components/NewNote";
 import { NoteCard } from "./components/NoteCard";
 
+const note = {
+  date: new Date(),
+  content: "Hello world",
+};
+
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 ">
       <form className="w-full ">
         <input
           type="text"
@@ -16,10 +21,8 @@ export function App() {
       <div className="h-px bg-slate-700"></div>
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px] ">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+
+        <NoteCard note={note} />
       </div>
     </div>
   );
